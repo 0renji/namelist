91244851ef5fec2f156f01b050b6dae18ac3184e
Christopher Kulks <christopher.kulks@insitu.de>
2020-09-04 14:20:06 +0200

    initial store commit;

---
+++ src/components/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{test}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainView",
+
+        computed: {
+            test() {
+                return this.$store.getters['global/test'];
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
